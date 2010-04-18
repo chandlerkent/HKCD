d1d361bd637370ae1b32e59bbb9328d908653907
Derek Hammer <derek.r.hammer@gmail.com>
2010-04-18 02:46:55 -0400

    Making main.j a javascript file. We are all javascript now!

--- Test/all-tests.js
+++ Test/all-tests.js
@@ -1,5 +1,6 @@
 exports.testNode = require("./node-test");
 exports.testClassTypes = require("./classtypes-test");
+exports.testParser = require("./parser-test");
 
 if (require.main === module)
     require("os").exit(require("test/runner").run(exports));
