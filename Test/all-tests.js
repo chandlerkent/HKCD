52bf5483cf6f41caf9e9b44b6bcb32af4e9b6de5
Derek Hammer <derek.r.hammer@gmail.com>
2010-04-21 00:05:26 -0400

    Adding tests to all-tests and cleaning up methodoverload (the override test is in methodoverride-test now). Also removed parser for all-tests because it takes a long time.

--- Test/all-tests.js
+++ Test/all-tests.js
@@ -1,7 +1,11 @@
 exports.testNode = require("./astnode-test");
 exports.testClassTypes = require("./classtypes-test");
-exports.testParser = require("./parser-test");
+//exports.testParser = require("./parser-test");
 exports.testDriver = require("./driver-test");
+exports.testFieldDecs = require("./fielddecs-test");
+exports.testFieldShadow = require("./fieldshadow-test");
+exports.testMethodOverload = require("./methodoverload-test");
+exports.testMethodOverride = require("./methodoverride-test");
 exports.testIntegration = require("./integration-test");
 
 if (require.main === module)
--- Test/methodoverload-test.js
+++ Test/methodoverload-test.js
@@ -14,26 +14,6 @@ exports.testThatTwoMethodsWithSameNameAreInvalid = function() {
     ASSERT.throwsError(function() {methodoverload.process(ast)});
 };
 
-exports.testThatOverriddenMethodFromSuperclassMustHaveSameArgsAndType = function() {
-    var ast = buildInvalidExtendsAST();
-    ASSERT.throwsError(function() {methodoverload.process(ast)});
-};
-
-function buildInvalidExtendsAST() {
-    return new ASTNode('Program', [
-        new ASTNode('MainClassDecl', [], { 'class_decl': 'Foo', 'param': 'args'}),
-        new ASTNode('ClassDecl', [
-            new ASTNode('MethodDecl', [3], { 'return_type': 'int', 'method_name': 'bar'}),
-            new ASTNode('MethodDecl', [false], { 'return_type': 'boolean', 'method_name': 'hasBar'})
-        ], { 'class_decl': 'Bar', 'extension': null}),
-        new ASTNode('ClassDecl', [
-            new ASTNode('MethodDecl', [42], { 'return_type': 'int', 'method_name': 'baz'}),
-            new ASTNode('MethodDecl', [true], { 'return_type': 'boolean', 'method_name': 'hasBaz'}),
-            new ASTNode('MethodDecl', [false], { 'return_type': 'boolean', 'method_name': 'bar',})
-        ], { 'class_decl': 'Baz', 'extension': 'Bar'})
-    ]);
-}
-
 function buildInvalidAST() {
     return new ASTNode('Program', [
         new ASTNode('MainClassDecl', [], { 'class_decl': 'Foo', 'param': 'args'}),
