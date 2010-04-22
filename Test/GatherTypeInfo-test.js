0aae1449980155c1fc4f7ac33c15fdc8b16f7d6c
Chandler Kent <ckentster@gmail.com>
2010-04-22 03:17:29 -0400

    Redone GatherTypeInfo with new data structures.

---
+++ Test/GatherTypeInfo-test.js
@@ -0,0 +1,32 @@
+var ASSERT = require("test/assert");
+var ASTNode = require("../lib/ASTNode").ASTNode;
+var GatherTypeInfo = require("../lib/GatherTypeInfo");
+var Environment = require("../lib/Environment").Environment;
+
+exports.testGatheringTypeInfo = function() {
+    var ast = new ASTNode("Program", [
+        new ASTNode("MainClassDecl", [], {"className": "Foo", "param": "args"}),
+        new ASTNode("ClassDecl", [
+            new ASTNode("ClassVarDecl", [], {"type": "int", "fieldName": "x"}),
+            new ASTNode("ClassVarDecl", [], {"type": "Foo", "fieldName": "foo"}),
+            new ASTNode("MethodDecl", [
+                new ASTNode('Formal', [], { 'type': "int", 'parameterName': "hello" })
+            ], {"returnType": "int", "methodName": "foo"}),
+            new ASTNode("MethodDecl", [], {"returnType": "boolean", "methodName": "foo"}),
+            new ASTNode("MethodDecl", [], {"returnType": "int", "methodName": "bar"}),
+        ], {"className": "Bar", "superClassName": null}),
+        new ASTNode("ClassDecl", [
+            new ASTNode("ClassVarDecl", [], {"type": "int", "fieldName": "x"}),
+            new ASTNode("ClassVarDecl", [], {"type": "boolean", "fieldName": "x"}),
+        ], {"className": "Bar", "superClassName": "Foo"}),
+        new ASTNode("ClassDecl", [
+            new ASTNode("ClassVarDecl", [], {"type": "int", "fieldName": "x"}),
+            new ASTNode("ClassVarDecl", [], {"type": "Bar", "fieldName": "z"}),
+        ], {"className": "Baz", "superClassName": "Oof"}),
+    ]);
+    
+    print(GatherTypeInfo.process(ast, new Environment()).env);
+};
+
+if (require.main === module)
+    require("os").exit(require("test/runner").run(exports));
