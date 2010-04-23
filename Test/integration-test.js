438626a93635853a24454a987a29112e17ea05f5
Derek Hammer <derek.r.hammer@gmail.com>
2010-04-23 15:42:50 -0400

    Making all of the integration tests pass. Woot!

--- Test/integration-test.js
+++ Test/integration-test.js
@@ -16,78 +16,78 @@ var MethodDecl = require("../lib/MethodDecl");
 var ASSERT = require("assert");
 var FileList = require("jake").FileList;
 
-// exports.testThatDuplicateClassesGetTypeChecked = function() {
-//     compilingFileResultsInError("Test/Files/TypeChecker/Ours/bad_class_decls.java", "Multiple declarations found for class Foo.");
-// };
-// 
-// exports.testThatDuplicateMethodDeclarationsFails = function() {
-//     compilingFileResultsInError("Test/Files/TypeChecker/Ours/bad_method_decls.java", "A method named bar has already been defined in the class Bar.");
-// };
-// 
-// exports.testThatOverridingSuperclassMethodDeclarationWithDifferentArgsOrTypeFails = function() {
-//     compilingFileResultsInError("Test/Files/TypeChecker/Ours/bad_method_decls.java", "A method named bar has already been defined in the class Bar.");
-// };
-// 
-// exports.testThatLegalClassDeclarationsDontFail = function() {
-//     compilingFileResultsInError("Test/Files/TypeChecker/Ours/good_class_decls.java");
-// };
-// 
-// exports.testThatLegalFieldDeclarationsDontFail = function() {
-//     compilingFileResultsInError("Test/Files/TypeChecker/Ours/good_field_decls.java");
-// };
-// 
-// exports.testThatLegalMethodDeclarationsDontFail = function() {
-//     compilingFileResultsInError("Test/Files/TypeChecker/Ours/good_method_decls.java");
-// };
-// 
-// exports.testThatDuplicateFieldDeclarationsFail = function() {
-//     compilingFileResultsInError("Test/Files/TypeChecker/Ours/bad_field_decls.java", "A field named x is defined more than once in Baz.");
-// };
-// 
-// exports.testThatTurnInFolderGoodsDontFail = function() {
-//     var files = new FileList("Test/Files/TypeChecker/Turn-In/good*.java").items();
-// 
-//     files.forEach(function(file) {
-//        compilingFileResultsInError(file); 
-//     });
-// };
-// 
-// exports.testThatTurnInFolderBadsFail = function() {
-//     var files = new FileList("Test/Files/TypeChecker/Turn-In/bad*.java").items();
-//     var expected = [
-//         "Multiple declarations found for class Foo.", 
-//         "Cannot extend the unknown superclass Baz.", 
-//         "The method {name: <size>, returnType: <boolean>, parameters: <[]>} in Baz attempts to override the method {name: <size>, returnType: <int>, parameters: <[]>} in Bar.", 
-//         "A field named x has already been defined in the superclass Bar.", 
-//         "A field named x is defined more than once in Bar.", 
-//         "A field named x has already been defined in the superclass Bar."
-//     ];
-//     
-//     files.forEach(function(file) {
-//        compilingFileResultsInError(file, expected[files.indexOf(file)]);
-//     });
-// };
-// 
-// exports.testThatEarlySamplesFail = function() {
-//     var files = new FileList("Test/Files/TypeChecker/EarlySamples/*.java").items();
-//     var expected = [
-//         "Multiple declarations found for class A.",
-//         "A field named c is initialized with an uninitialized type C.",
-//         "The parameter c is initialized with undefined type C.",
-//         "The method {name: <start>, returnType: <boolean>, parameters: <[]>} in B attempts to override the method {name: <start>, returnType: <int>, parameters: <[]>} in C.",
-//         "A field named a has already been defined in the superclass noPoint.",
-//         "A method named setB has already been defined in the class noPoint2.",
-//         "A field named a is defined more than once in noPoint.",
-//         "Cannot extend the unknown superclass Foo1.",
-//         "Cannot extend the unknown superclass Foo.",
-//     ];
-//     
-//     files.forEach(function(file) {
-//         compilingFileResultsInError(file, expected[files.indexOf(file)]);
-//     });
-// };
+exports.testThatDuplicateClassesGetTypeChecked = function() {
+    compilingFileResultsInError("Test/Files/TypeChecker/Ours/bad_class_decls.java", "Multiple declarations found for class Foo.");
+};
 
-exports.testThatFullTestCasesFail = function() {
+exports.testThatDuplicateMethodDeclarationsFails = function() {
+    compilingFileResultsInError("Test/Files/TypeChecker/Ours/bad_method_decls.java", "A method named bar has already been defined in the class Bar.");
+};
+
+exports.testThatOverridingSuperclassMethodDeclarationWithDifferentArgsOrTypeFails = function() {
+    compilingFileResultsInError("Test/Files/TypeChecker/Ours/bad_method_decls.java", "A method named bar has already been defined in the class Bar.");
+};
+
+exports.testThatLegalClassDeclarationsDontFail = function() {
+    compilingFileResultsInError("Test/Files/TypeChecker/Ours/good_class_decls.java");
+};
+
+exports.testThatLegalFieldDeclarationsDontFail = function() {
+    compilingFileResultsInError("Test/Files/TypeChecker/Ours/good_field_decls.java");
+};
+
+exports.testThatLegalMethodDeclarationsDontFail = function() {
+    compilingFileResultsInError("Test/Files/TypeChecker/Ours/good_method_decls.java");
+};
+
+exports.testThatDuplicateFieldDeclarationsFail = function() {
+    compilingFileResultsInError("Test/Files/TypeChecker/Ours/bad_field_decls.java", "A field named x is defined more than once in Baz.");
+};
+
+exports.testThatTurnInFolderGoodsDontFail = function() {
+    var files = new FileList("Test/Files/TypeChecker/Turn-In/good*.java").items();
+
+    files.forEach(function(file) {
+       compilingFileResultsInError(file); 
+    });
+};
+
+exports.testThatTurnInFolderBadsFail = function() {
+    var files = new FileList("Test/Files/TypeChecker/Turn-In/bad*.java").items();
+    var expected = [
+        ["Multiple declarations found for class Foo.", "Cannot extend the unknown superclass Bar."], 
+        "Cannot extend the unknown superclass Baz.", 
+        "The method {name: <size>, returnType: <boolean>, parameters: <[]>} in Baz attempts to override the method {name: <size>, returnType: <int>, parameters: <[]>} in Bar.", 
+        "A field named x has already been defined in the superclass Bar.", 
+        "A field named x is defined more than once in Bar.", 
+        "A field named x has already been defined in the superclass Bar."
+    ];
+    
+    files.forEach(function(file) {
+       compilingFileResultsInError(file, expected[files.indexOf(file)]);
+    });
+};
+
+exports.testThatEarlySamplesFail = function() {
+    var files = new FileList("Test/Files/TypeChecker/EarlySamples/*.java").items();
+    var expected = [
+        "Multiple declarations found for class A.",
+        "A field named c is initialized with an uninitialized type C.",
+        "The parameter c is initialized with undefined type C.",
+        "The method {name: <start>, returnType: <boolean>, parameters: <[]>} in B attempts to override the method {name: <start>, returnType: <int>, parameters: <[]>} in C.",
+        "A field named a has already been defined in the superclass noPoint.",
+        "A method named setB has already been defined in the class noPoint2.",
+        "A field named a is defined more than once in noPoint.",
+        "Cannot extend the unknown superclass Foo1.",
+        "Cannot extend the unknown superclass Foo.",
+    ];
+    
+    files.forEach(function(file) {
+        compilingFileResultsInError(file, expected[files.indexOf(file)]);
+    });
+};
+
+exports.testThatFullTestCasesGiveExpectedOutput = function() {
     var files = new FileList("Test/Files/TypeChecker/FullTestCases/FullTests/*.java").items();
     var expected = [
         "Multiple declarations found for class A.",
@@ -151,65 +151,98 @@ exports.testThatFullTestCasesFail = function() {
             "A field named b has already been defined in the superclass Test3b.",
         ],
         "A field named x is defined more than once in Test4a.",
-        "HERE",
+        "Cannot extend the unknown superclass Test5c.",
+        "Multiple declarations found for class Test6b.",
+        "Cannot extend the unknown superclass MissingClass.",
+        [
+            "The method {name: <test>, returnType: <int>, parameters: <[{name: <a>, type: <int>}, {name: <b>, type: <int>}, {name: <c>, type: <int>}]>} in Test8b attempts to override the method {name: <test>, returnType: <int>, parameters: <[{name: <a>, type: <int>}, {name: <b>, type: <int>}]>} in Test8a.",
+            "The method {name: <test>, returnType: <boolean>, parameters: <[{name: <a>, type: <int>}, {name: <b>, type: <int>}]>} in Test8c attempts to override the method {name: <test>, returnType: <int>, parameters: <[{name: <a>, type: <int>}, {name: <b>, type: <int>}]>} in Test8a."
+        ],
+        "Multiple declarations found for class a.",
+        [
+            "The method {name: <getNum>, returnType: <boolean>, parameters: <[]>} in d attempts to override the method {name: <getNum>, returnType: <int>, parameters: <[]>} in c.",
+            "The method {name: <getNum>, returnType: <boolean>, parameters: <[]>} in d attempts to override the method {name: <getNum>, returnType: <int>, parameters: <[]>} in b."
+        ],
+        "",
+        "",
+        "A field named soboring is initialized with an uninitialized type bool.",
+        "The method {name: <tni>, returnType: <int>, parameters: <[{name: <cilbup>, type: <int>}]>} in um attempts to override the method {name: <tni>, returnType: <ssalc>, parameters: <[{name: <cilbup>, type: <int>}]>} in ssalc.",
+        "Multiple declarations found for class foo.",
+        [
+            "There was a cycle detected.",
+            "Cannot extend the unknown superclass baz."
+        ],
+        [
+            "Cannot extend the unknown superclass baz.",
+            "The method {name: <test>, returnType: <boolean>, parameters: <[]>} in bar attempts to override the method {name: <test>, returnType: <int>, parameters: <[]>} in baz."
+        ],
+        "",
+        "A field named test is defined more than once in baz.",
         "",
         "",
         "",
         "",
         "",
+        "A field named isOrange has already been defined in the superclass BallClass.",
+        "Cannot extend the unknown superclass bird.",
+        "The method {name: <returnsFive>, returnType: <boolean>, parameters: <[]>} in OneMethodDos attempts to override the method {name: <returnsFive>, returnType: <int>, parameters: <[]>} in OneMethod.",
+        "Multiple declarations found for class KnightsWhoSayNi.",
+        "A field named number is defined more than once in SecondClass.",
+        "A method named dupe has already been defined in the class crazy.",
+        "",
+        "Multiple declarations found for class sample2.",
+        "Cannot extend the unknown superclass mobily.",
+        "Cannot extend the unknown superclass mobily.",
+        [
+            "The method {name: <setsms>, returnType: <int>, parameters: <[{name: <x>, type: <boolean>}]>} in zain attempts to override the method {name: <setsms>, returnType: <int>, parameters: <[{name: <x>, type: <int>}]>} in stc.",
+            "The method {name: <setDSL>, returnType: <int>, parameters: <[{name: <x>, type: <boolean>}]>} in zain attempts to override the method {name: <setDSL>, returnType: <int>, parameters: <[{name: <x>, type: <int>}]>} in stc."
+        ],
+        [
+            "The method {name: <setsms>, returnType: <int>, parameters: <[{name: <x>, type: <boolean>}]>} in mobily attempts to override the method {name: <setsms>, returnType: <int>, parameters: <[{name: <x>, type: <int>}]>} in stc.",
+            "The method {name: <setDSL>, returnType: <int>, parameters: <[{name: <x>, type: <boolean>}]>} in mobily attempts to override the method {name: <setDSL>, returnType: <int>, parameters: <[{name: <x>, type: <int>}]>} in stc.",
+            "A field named z is initialized with an uninitialized type Zain."
+        ],
+        "A field named DSL has already been defined in the superclass mobily.",
+        "A method named getDSL has already been defined in the class mobily.",
+        "",
+        "A method named myMethod has already been defined in the class AnotherClass.",
+        "",
+        [
+            "A field named a is defined more than once in SomeClass.",
+            "A field named a is initialized with an uninitialized type Type."
+        ],
+        "Multiple declarations found for class MyClass.",
+        [
+            "A method named myMethod has already been defined in the class SomeClass.",
+            "A method named myMethod has already been defined in the class SomeOtherClass.",
+            "The method {name: <myMethod>, returnType: <boolean>, parameters: <[{name: <b>, type: <int>}]>} in SomeOtherClass attempts to override the method {name: <myMethod>, returnType: <int>, parameters: <[{name: <a>, type: <int>}]>} in SomeClass.",
+        ],
+        "A method named myMethod has already been defined in the class SomeClass.",
+        [
+            "A method named myMethod has already been defined in the class SomeClass.",
+            "The method {name: <myMethod>, returnType: <boolean>, parameters: <[{name: <a>, type: <int>}]>} in AClass attempts to override the method {name: <myMethod>, returnType: <boolean>, parameters: <[{name: <b>, type: <boolean>}]>} in SomeClass."
+        ],
+        "The method {name: <estimateNumberOfZombies>, returnType: <int>, parameters: <[{name: <limbs>, type: <int>}, {name: <heads>, type: <int>}]>} in Cred attempts to override the method {name: <estimateNumberOfZombies>, returnType: <int>, parameters: <[{name: <limbs>, type: <int>}]>} in Ed.",
+        "Cannot extend the unknown superclass Ced.",
+        "A field named shells has already been defined in the superclass Ed.",
+        [
+            "Cannot extend the unknown superclass Zed.",
+            "Cannot extend the unknown superclass Ced."
+        ],
+        "The return type Joe of method named estimateNumberOfZombies is undefined.",
+        "Cannot extend the unknown superclass Object.",
+        "",
+        "",
+        "A method named doCoke has already been defined in the class TryDrugs.",
+        "The method {name: <program>, returnType: <Definition>, parameters: <[]>} in VerbDefinition attempts to override the method {name: <program>, returnType: <int>, parameters: <[]>} in Definition.",
+        "A field named lethal is defined more than once in YourPrescription.",
+        "The method {name: <attachedToTheEars>, returnType: <int>, parameters: <[]>} in CourtroomDog attempts to override the method {name: <attachedToTheEars>, returnType: <boolean>, parameters: <[]>} in WhereWasTheDog.",
+        "Multiple declarations found for class StatueOfLiberty.",
+        "The method {name: <doRubiksCube>, returnType: <int>, parameters: <[]>} in StevieWonder attempts to override the method {name: <doRubiksCube>, returnType: <boolean>, parameters: <[]>} in Dude.",
         "",
         "",
         "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
+        "Multiple declarations found for class Test7a.",
         ""
     ];
     
