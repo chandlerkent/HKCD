81d75e503f3344087c23f111c0e4a8d52c329fe9
Chandler Kent <ckentster@gmail.com>
2010-04-26 22:22:53 -0400

    Subtract Expressions for return type.

--- Test/astbuilder.js
+++ Test/astbuilder.js
@@ -30,52 +30,56 @@ exports.buildValidAST = function() {
 
 var ProgramNode = exports.ProgramNode = function() {
     return new ASTNode("Program",[]);
-}
+};
 
 var MainClassNode = exports.MainClassNode = function() {
     return new ASTNode('MainClassDecl', [], { 'className': 'SomeClassWeNeverUse', 'param': 'args', 'superClassName': null });
-}
+};
 
 var ClassNode = exports.ClassNode = function(name, superclass) {
     return new ASTNode('ClassDecl', [], { 'className': name, 'superClassName': superclass ? superclass : null });
-}
+};
 
 var FieldNode = exports.FieldNode = function(name, type) {
     return new ASTNode('ClassVarDecl', [], { 'type': type, 'fieldName': name });
-}
+};
 
 var MethodNode = exports.MethodNode = function(name, type ) {
     return new ASTNode('MethodDecl', [], { 'returnType': type, 'methodName': name });
-}
+};
 
 var ParameterNode = exports.ParameterNode = function(name, type) {
     return new ASTNode('Formal', [], { 'type': type, 'parameterName': name });
-}
+};
 
 var IntegerExpression = exports.IntegerExpression = function(intValue) {
     return new ASTNode('IntegerExpression', [], {'expression': intValue, 'checkType': ASTNode.expressionCheck.Integer });
-}
+};
 
 var AddExpression = exports.AddExpression = function(leftHandSide, rightHandSide) {
     return new ASTNode('Add', [leftHandSide, rightHandSide], { 'checkType': ASTNode.expressionCheck.Add });
-}
+};
+
+var SubtractExpression = exports.SubtractExpression = function(leftHandSide, rightHandSide) {
+    return new ASTNode('Subtract', [leftHandSide, rightHandSide], { 'checkType': ASTNode.expressionCheck.Subtract });
+};
 
 var TrueExpression = exports.TrueExpression = function() {
     return new ASTNode('TrueExpression', [], { 'checkType': ASTNode.expressionCheck.True });
-}
+};
 
 var FalseExpression = exports.FalseExpression = function() {
     return new ASTNode('FalseExpression', [], { 'checkType': ASTNode.expressionCheck.False });
-}
+};
 
 var NullExpression = exports.NullExpression = function() {
     return new ASTNode('NullExpression', [], { 'checkType': ASTNode.expressionCheck.Null });
-}
+};
 
 var ThisExpression = exports.ThisExpression = function() {
     return new ASTNode('ThisExpression', [], { 'checkType': ASTNode.expressionCheck.This });
-}
+};
 
 var ConstructExpression = exports.ConstructExpression = function(type) {
     return new ASTNode('ConstructExpression', [], { 'constructor':type, 'checkType': ASTNode.expressionCheck.Construct });
-}
+};
