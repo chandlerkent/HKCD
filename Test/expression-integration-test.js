096f93e8532de77f34a4ab2b4e8bdc2a0c1a72cf
Chandler Kent <ckentster@gmail.com>
2010-04-29 14:56:45 -0400

    Making the test cases run no matter what and generate a test for each file. This is pretty cool!

--- Test/expression-integration-test.js
+++ Test/expression-integration-test.js
@@ -3,30 +3,34 @@ var TypeChecker = require("../lib/TypeChecker");
 var Parser = require("../lib/parser").Parser;
 var ASSERT = require("assert");
 
-exports.testThatEarlySamplesEqualExpectedOutput = function() {
-    var pathOfInputFiles = "Test/Files/ExpressionTypeChecker/EarlySamples/*.java";
-    var pathOfOutputFiles = "Test/Files/ExpressionTypeChecker/EarlySamples/*.out";
-    
-    var inputFiles = new FileList(pathOfInputFiles).items();
-    var outputFiles = new FileList(pathOfOutputFiles).items();
-    
-    for(var i = 0; i < inputFiles.length; i++) {
-        print("TypeChecking " + inputFiles[i]);
-        
-        var parser = new Parser(readGrammarFromFile("src/grammar.json"));
-        var ast = parser.parse(readFile(inputFiles[i]));
+var inFiles = new FileList("Test/Files/ExpressionTypeChecker/EarlySamples/*.java").items();
+var outFiles = new FileList("Test/Files/ExpressionTypeChecker/EarlySamples/*.out").items();
 
+for (var i = 0; i < inFiles.length; i++) {
+    var fileParts = inFiles[i].split("/");
+    fileParts.shift();
+    fileParts.shift();
+    var fileNameThatMatters = fileParts.join("/");
+    exports["testFile__/" + fileNameThatMatters] = buildTestFunctionForFile(inFiles[i], outFiles[i]);
+}
+
+var parser = new Parser(readGrammarFromFile("src/grammar.json"));
+
+function buildTestFunctionForFile(inFile, outFile) {
+    return function() {
+        var ast = parser.parse(readFile(inFile));
+        
         var env = TypeChecker.typeCheck(ast).env;
 
         var actualMessages = env.errors.map(function(err) {
             return err.message;
         }).join("\n");
         
-        var expected = readFile(outputFiles[i]);
+        var expected = readFile(outFile);
         
         ASSERT.equal(expected, actualMessages, "Error message: <" + actualMessages + 
             ">\n\ndid not match\n\n<" + expected + ">");
-    }
+    };
 }
 
 function readGrammarFromFile(filePath) {
