4ad246b1252763719414f7fc4980ef74fd82127f
Chandler Kent <ckentster@gmail.com>
2010-05-07 11:38:13 -0400

    Adding more to precedence test.

--- Test/Files/CodeGen/Ours/precedence.java
+++ Test/Files/CodeGen/Ours/precedence.java
@@ -1,5 +1,5 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println(1 + 2 * 3 + 4 - 4 / 2 - - 3); // 12
+        System.out.println(1 + -2 * 3 + 4 - 4 / 2 + - 3); // -6
     }
 }
