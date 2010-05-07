ac4daf4cfd559eeb2175f4a488f2071fe43757a6
Chandler Kent <ckentster@gmail.com>
2010-05-07 11:19:11 -0400

    Adding while, local variable assignment, and local variable lookup (this is experimental-should probably be doing stuff with the environment?).

---
+++ Test/Files/CodeGen/Ours/while.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int count = 42;
+        int i = 1;
+        while (i < count) {
+            System.out.println(i);
+            i = i + 1;
+        }
+    }
+}
