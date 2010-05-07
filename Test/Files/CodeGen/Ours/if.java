29ae108f2bfb549adcd501cc4088673aacdb7df2
Chandler Kent <ckentster@gmail.com>
2010-05-07 01:33:19 -0400

    Basic pass at if.

---
+++ Test/Files/CodeGen/Ours/if.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        if (1 < 2) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
