979c335d5e24ac2fee3a7a7cda0eeb960c352631
Chandler Kent <ckentster@gmail.com>
2010-05-07 11:23:59 -0400

    Adding the comparator operators for lte, gt, gte.

--- Test/Files/CodeGen/Ours/while.java
+++ Test/Files/CodeGen/Ours/while.java
@@ -1,10 +1,10 @@
 class Main {
     public static void main(String[] args) {
-        int count = 42;
-        int i = 1;
-        while (i < count) {
+        int count = 1;
+        int i = 42;
+        while (i >= count) {
             System.out.println(i);
-            i = i + 1;
+            i = i - 1;
         }
     }
 }
