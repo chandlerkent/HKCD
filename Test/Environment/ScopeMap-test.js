1ae144b4af632dc936db1c4a2f9797ddd00a0c84
Derek Hammer <derek.r.hammer@gmail.com>
2010-05-08 18:23:19 -0400

    ScopeMap tests

--- Test/Environment/ScopeMap-test.js
+++ Test/Environment/ScopeMap-test.js
@@ -1,5 +1,34 @@
 var ScopeMap = require("../../lib/Environment").ScopeMap;
 var ASSERT = require("test/assert");
+var VariableMap = require("../../lib/Environment").VariableMap;
+
+exports.testThatScopeMapInitializes = function() {
+    var scope = new ScopeMap();
+    
+    ASSERT.eq("[]", scope.toString());
+};
+
+exports.testThatScopeMapAddVariable = function() {
+    var scope = new ScopeMap();
+    var variable = new VariableMap("x", "int");
+    
+    scope.addVariable(variable);
+    
+    ASSERT.isTrue(scope.hasVariable("x"));
+    
+    ASSERT.eq(variable, scope.getVariable("x"));
+};
+
+exports.testThatScopeMapCopies = function() {
+    var scope = new ScopeMap();
+    var variable = new VariableMap("x", "int");
+    
+    scope.addVariable(variable);
+    
+    var copy = scope.copy();
+    
+    ASSERT.eq("[{name: <x>, type: <int>, value: <null>}]", copy.toString());
+}
 
 if (require.main === module)
     require("os").exit(require("test/runner").run(exports));
