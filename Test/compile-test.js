94e5caf7db41466559402416c36d94a106b59e80
Derek Hammer <derek.r.hammer@gmail.com>
2010-05-10 16:16:20 -0400

    Line Endings and given output for tests

--- Test/compile-test.js
+++ Test/compile-test.js
@@ -2,14 +2,31 @@ var Compiler = require("../lib/Compiler.js").Compiler;
 var ASSERT = require("test/assert");
 var OS = require("os");
 var FILE = require("file");
+var FileList = require("jake").FileList;
 
-var options = {
-    "debug":false,
-    "args":["Test/Files/CodeGen/Ours/basic.java"]
-};
+var files = [];
+var outputs = [];
 
-exports.testThatBasicRuns = function() {
-    runCompileTest("Test/Files/CodeGen/Ours/basic.java", "0\n");
+(new FileList("Test/Files/CodeGen/Ours/*.java").items()).forEach(function(file) {
+    files.push(file);
+});
+(new FileList("Test/Files/CodeGen/Ours/*.out").items()).forEach(function(file) {
+    outputs.push(file);
+});
+
+(new FileList("Test/Files/CodeGen/Turn-In/*.java").items()).forEach(function(file) {
+    files.push(file);
+});
+(new FileList("Test/Files/CodeGen/Turn-In/*.out").items()).forEach(function(file) {
+    outputs.push(file);
+});
+
+var buildCompileTest = function(file, expectedOutputFile) {
+    var expectedOutput = FILE.read(expectedOutputFile);
+    
+    return function() {
+        runCompileTest(file, expectedOutput);
+    };
 };
 
 var runCompileTest = function(file, expectedOutput) {
@@ -34,6 +51,14 @@ var runCompileTest = function(file, expectedOutput) {
     FILE.rmtree("Main.class");
     FILE.rmtree(compiledFile);
 };
+
+for (var i = 0; i < files.length; i++) {
+    var fileParts = files[i].split("/");
+    fileParts.shift();
+    fileParts.shift();
+    var fileNameThatMatters = fileParts.join("/");
+    exports["testFile__/" + fileNameThatMatters] = buildCompileTest(files[i], outputs[i]);
+}
 
 if (require.main === module)
     require("os").exit(require("test/runner").run(exports));
