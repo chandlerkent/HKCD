c8b03db2c08ff9b52bc87d4d4656899a14f3093b
Chandler Kent <ckentster@gmail.com>
2010-05-11 01:23:44 -0400

    Some clean-up.

--- Test/Files/CodeGen/Ours/classes.java
+++ Test/Files/CodeGen/Ours/classes.java
@@ -1,18 +1,14 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println((new Foo()).foo(6));
+        System.out.println((new Foo()).foo(5, 7, 2));
         // System.out.println((new Bar()).bar());
     }
 }
 
 class Foo {
-    public int foo(int x) {
-        return x * 7;
+    int z;
+    int y;
+    public int foo(int x, int y, int z) {
+        return x * y + z;
     }
 }
-
-// class Bar extends Foo {
-//     public int bar() {
-//         return this.foo(6);
-//     }
-// }
