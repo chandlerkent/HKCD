7b0ed490ab6aef24e50133b8ed40f3c36ad77806
Chandler Kent <ckentster@gmail.com>
2010-05-11 21:06:20 -0400

    Adding correct scoping so we can add multiple classes. Added this lookup.

--- Test/Files/CodeGen/Ours/classes.java
+++ Test/Files/CodeGen/Ours/classes.java
@@ -1,14 +1,18 @@
 class Main {
     public static void main(String[] args) {
         System.out.println((new Foo()).foo(5, 7, 2));
-        // System.out.println((new Bar()).bar());
+        System.out.println((new Bar()).bar());
     }
 }
 
 class Foo {
-    int z;
-    int y;
     public int foo(int x, int y, int z) {
         return x * y + z;
+    }
+}
+
+class Bar extends Foo {
+    public int bar() {
+        return this.foo(5, 7, 2);
     }
 }
