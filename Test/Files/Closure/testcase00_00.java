29a720f8122ec83a60b8a1066d1b28edc3269270
Chandler Kent <ckentster@gmail.com>
2010-05-16 15:45:11 -0400

    A more complete test case and closure evaluation in grammar.

--- Test/Files/Closure/testcase00_00.java
+++ Test/Files/Closure/testcase00_00.java
@@ -1,14 +1,20 @@
+class Main {
+    public static void main( String[] args ) {
+        int x = 3;
+        int y = 4;
 
-class Main
-{
-   public static void main( String[] args )
-   {
-      // Function test = { int x, int y | x + y };
-      Function test2 = { | return x * y; };
-      Function test3 = { boolean y |
-          int four = x + y;
-          boolean five = y + z;
-          return 3;
-      };
-   }
+        Function test1 = { | return x * y; };
+        
+        Function test2 = { int y |
+            int b = x + y;
+            return b;
+        };
+
+        int answer1 = test1();
+        int answer2 = test2(3);
+
+        System.out.println(answer1); // 12
+        System.out.println(answer2); // 6
+        System.out.println(test1() + test2(27)); // 42
+    }
 }
