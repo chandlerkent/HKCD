c44302dbded9a1eba578c7f54d8caa9388a64704
Chandler Kent <ckentster@gmail.com>
2010-05-16 15:27:35 -0400

    Closure grammar (w/o implicit return) and test case.

--- Test/Files/Closure/testcase00_00.java
+++ Test/Files/Closure/testcase00_00.java
@@ -3,6 +3,12 @@ class Main
 {
    public static void main( String[] args )
    {
-      Function test = { int x, int y | x + y };
+      // Function test = { int x, int y | x + y };
+      Function test2 = { | return x * y; };
+      Function test3 = { boolean y |
+          int four = x + y;
+          boolean five = y + z;
+          return 3;
+      };
    }
 }
