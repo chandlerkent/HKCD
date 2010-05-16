50b1f7f53c4c6fbedd91c26a4b7f78a07e8fa2a1
Chandler Kent <ckentster@gmail.com>
2010-05-16 18:22:43 -0400

    First pass at type checking closure declarations.

--- Test/Files/Closure/testcase00_00.java
+++ Test/Files/Closure/testcase00_00.java
@@ -3,18 +3,18 @@ class Main {
         int x = 3;
         int y = 4;
 
-        Function test1 = { | return x * y; };
+        Function test1 = { int z | int a = z; return x * y * z * a; };
         
         Function test2 = { int y |
             int b = x + y;
             return b;
         };
 
-        int answer1 = test1();
-        int answer2 = test2(3);
-
-        System.out.println(answer1); // 12
-        System.out.println(answer2); // 6
-        System.out.println(test1() + test2(27)); // 42
+        // int answer1 = test1();
+        // int answer2 = test2(3);
+        // 
+        // System.out.println(answer1); // 12
+        // System.out.println(answer2); // 6
+        // System.out.println(test1() + test2(27)); // 42
     }
 }
