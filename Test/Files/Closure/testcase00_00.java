2ddd689c2c69acb7f84709855b0ddfddde54b140
Chandler Kent <ckentster@gmail.com>
2010-05-17 20:50:23 -0400

    Work from last time.

--- Test/Files/Closure/testcase00_00.java
+++ Test/Files/Closure/testcase00_00.java
@@ -3,16 +3,16 @@ class Main {
         int x = 3;
         int y = 4;
 
-        Function test1 = { int z | int a = z; return x * y * z * a; };
+        Function test1 = { | return x * y; };
         
         Function test2 = { int y |
             int b = x + y;
             return b;
         };
 
-        // int answer1 = test1();
-        // int answer2 = test2(3);
-        // 
+        int answer1 = test1();
+        int answer2 = test2(3);
+        
         // System.out.println(answer1); // 12
         // System.out.println(answer2); // 6
         // System.out.println(test1() + test2(27)); // 42
