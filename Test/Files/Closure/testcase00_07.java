26750b92aacbafacc94b2cae244a75797bf36d50
Derek Hammer <derek.r.hammer@gmail.com>
2010-05-18 06:39:22 -0400

    A little more interesting test case

--- Test/Files/Closure/testcase00_07.java
+++ Test/Files/Closure/testcase00_07.java
@@ -2,15 +2,29 @@ class Main {
     public static void main( String[] args ) {
         Function x = { int a | return a + a; };
         Function y = { int a | return a * a; };
+        Function z = { | return true; };
         
         Foo foo = new Foo();
+        int w = 0;
+        
+        if(foo.boo(z)) {
+            w = 42;
+        } else {
+            w = -1;
+        }
+        
         System.out.println(foo.foo(x));
         System.out.println(foo.foo(y));
+        System.out.println(w);
     }
 }
 
 class Foo {
     public int foo(Function x) {
         return x(7);
+    }
+    
+    public boolean boo(Function y) {
+        return y();
     }
 }
