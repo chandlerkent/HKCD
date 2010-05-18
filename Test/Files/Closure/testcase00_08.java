4bd9a41dee6f0c5299f507950592c609161859d2
Derek Hammer <derek.r.hammer@gmail.com>
2010-05-18 12:36:16 -0400

    Support for inline closures!

---
+++ Test/Files/Closure/testcase00_08.java
@@ -0,0 +1,29 @@
+class Main {
+    public static void main( String[] args ) {
+        Function x = { int a | return a + a; };
+        Function y = { int a | return a * a; };
+        
+        Foo foo = new Foo();
+        int w = 0;
+        
+        if(foo.boo({ | return true; })) {
+            w = 42;
+        } else {
+            w = -1;
+        }
+        
+        System.out.println(foo.foo(x));
+        System.out.println(foo.foo(y));
+        System.out.println(w);
+    }
+}
+
+class Foo {
+    public int foo(Function x) {
+        return x(7);
+    }
+    
+    public boolean boo(Function y) {
+        return y();
+    }
+}
